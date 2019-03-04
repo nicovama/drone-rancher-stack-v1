084d4842484fd7b2771cfb68363300b77b6f09d9
nicovama <nicovama@gmail.com>
2019-03-04 16:30:14 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -17,4 +17,4 @@ echo ${SECRETKEY}
 echo ${PLUGIN_URL}
 echo "Stoping the Stack"
 echo "${PLUGIN_URL} ${PLUGIN_ACCESSKEY} ${PLUGIN_SECRETKEY} ${PLUGIN_STACK}"
-/bin/rancher --url ${PLUGIN_URL} --access-key ${PLUGIN_ACCESSKEY} --secret-key ${PLUGIN_SECRETKEY} stop --type ${PLUGIN_STACK} 
+/bin/rancher --url ${PLUGIN_URL} --access-key ${PLUGIN_ACCESSKEY} --secret-key ${PLUGIN_SECRETKEY} stop ${PLUGIN_STACK} 
