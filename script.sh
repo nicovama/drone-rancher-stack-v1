6dd5b55e1d3cfccbf8730f527dd561e76408c9e9
nicovama <nicovama@gmail.com>
2019-03-08 17:31:46 +0900

    Add rollback command

--- script.sh
+++ script.sh
@@ -11,15 +11,5 @@ fi
 if [[ -n $RANCHER_SECRETKEY ]]; then
     SECRETKEY="$RANCHER_SECRETKEY"
 fi
-echo "rancher-compose.yml @ ${RANCHER_COMPOSE}" 
-cat ${RANCHER_COMPOSE}
-echo ""
-echo "docker-compose.yml @ ${DOCKER_COMPOSE}"
-cat ${DOCKER_COMPOSE}
-echo ""
-echo ${ACCESSKEY}
-echo ${SECRETKEY}
-echo ${PLUGIN_URL}
-echo "Deploying Rancher Stack with force upgrade"
-echo "${PLUGIN_URL} ${PLUGIN_ACCESSKEY} ${PLUGIN_SECRETKEY} ${PLUGIN_STACK}"
-/bin/rancher --url ${PLUGIN_URL} --access-key ${PLUGIN_ACCESSKEY} --secret-key ${PLUGIN_SECRETKEY} up --stack ${PLUGIN_STACK} -d -f ${DOCKER_COMPOSE} --rancher-file ${RANCHER_COMPOSE} --pull --force-recreate --confirm-upgrade
+echo "Rollback Rancher stack to previous configuration"
+/bin/rancher --url ${PLUGIN_URL} --access-key ${PLUGIN_ACCESSKEY} --secret-key ${PLUGIN_SECRETKEY} up --stack ${PLUGIN_STACK} -d -r
