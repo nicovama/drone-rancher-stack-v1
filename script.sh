3ec759f0ea286090979a435da947953309c05431
nicovama <nicovama@gmail.com>
2019-03-14 18:03:59 +0900

    add conditional for tests and revert

--- script.sh
+++ script.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
-if [[ -n $PLUGIN_ACCESSKEY ]]; then
-    ACCESSKEY="$PLUGIN_ACCESSKEY"
-fi
-if [[ -n $RANCHER_ACCESSKEY ]]; then
-    ACCESSKEY="$RANCHER_ACCESSKEY"
-fi
-if [[ -n $PLUGIN_SECRETKEY ]]; then
-    SECRETKEY="$PLUGIN_SECRETKEY"
-fi
-if [[ -n $RANCHER_SECRETKEY ]]; then
-    SECRETKEY="$RANCHER_SECRETKEY"
-fi
+
+pytest test_healthcheck.tavern.yaml --elasticsearch_url=$ES_URL --logstash_url=$LOG2_URL --logstash_url=$LOG2_URL --kibana_url=$KIB_URL -vv
+
+rc=$?
+if [ $rc -ne 0 ]; then
+/bin/rancher --url ${PLUGIN_URL} --access-key ${PLUGIN_ACCESSKEY} --secret-key ${PLUGIN_SECRETKEY} up --stack ${PLUGIN_STACK} --confirm-upgrade
+else
 echo "Rollback Rancher stack to previous configuration"
 /bin/rancher --url ${PLUGIN_URL} --access-key ${PLUGIN_ACCESSKEY} --secret-key ${PLUGIN_SECRETKEY} up --stack ${PLUGIN_STACK} -d --rancher-file ${RANCHER_COMPOSE} --upgrade --rollback
+fi
+echo "Deployment complete"
