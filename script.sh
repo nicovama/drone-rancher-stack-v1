dc98e927fe467a6cf06afdff93ae7172335eeae6
nicovama <nicovama@gmail.com>
2019-03-14 16:39:38 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
-if [[ -n $PLUGIN_ACCESSKEY ]]; then
-    ACCESSKEY="$PLUGIN_ACCESSKEY"
-fi
-if [[ -n $RANCHER_ACCESSKEY ]]; then
-    ACCESSKEY="$RANCHER_ACCESSKEY"
-fi
-if [[ -n $PLUGIN_SECRETKEY ]]; then
-    SECRETKEY="$PLUGIN_SECRETKEY"
-fi
-if [[ -n $RANCHER_SECRETKEY ]]; then
-    SECRETKEY="$RANCHER_SECRETKEY"
 fi
 echo "rancher-compose.yml @ ${RANCHER_COMPOSE}" 
 cat ${RANCHER_COMPOSE}
@@ -23,3 +12,4 @@ echo ${PLUGIN_URL}
 echo "Deploying Rancher Stack with force upgrade"
 echo "${PLUGIN_URL} ${PLUGIN_ACCESSKEY} ${PLUGIN_SECRETKEY} ${PLUGIN_STACK}"
 /bin/rancher --url ${PLUGIN_URL} --access-key ${PLUGIN_ACCESSKEY} --secret-key ${PLUGIN_SECRETKEY} up --stack ${PLUGIN_STACK} -d -f ${DOCKER_COMPOSE} --rancher-file ${RANCHER_COMPOSE} --pull --force-recreate
+/bin/rancher --url ${PLUGIN_URL} --access-key ${PLUGIN_ACCESSKEY} --secret-key ${PLUGIN_SECRETKEY} up --stack ${PLUGIN_STACK} -d -f ${DOCKER_COMPOSE_LB} --rancher-file ${RANCHER_COMPOSE_LB} --pull --force-recreate
