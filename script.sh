c01c8fe51e9c5c4cbbf9a1581ed29a27796f4c7f
nicovama <nicovama@gmail.com>
2019-03-14 16:57:54 +0900

    Add loadbalancer to deployment

--- script.sh
+++ script.sh
@@ -12,4 +12,6 @@ echo ${PLUGIN_URL}
 echo "Deploying Rancher Stack with force upgrade"
 echo "${PLUGIN_URL} ${PLUGIN_ACCESSKEY} ${PLUGIN_SECRETKEY} ${PLUGIN_STACK}"
 /bin/rancher --url ${PLUGIN_URL} --access-key ${PLUGIN_ACCESSKEY} --secret-key ${PLUGIN_SECRETKEY} up --stack ${PLUGIN_STACK} -d -f ${DOCKER_COMPOSE} --rancher-file ${RANCHER_COMPOSE} --pull --force-recreate
+sleep 2m
+echo "Deploying Load Balancer"
 /bin/rancher --url ${PLUGIN_URL} --access-key ${PLUGIN_ACCESSKEY} --secret-key ${PLUGIN_SECRETKEY} up --stack ${PLUGIN_STACK} -d -f ${DOCKER_COMPOSE_LB} --rancher-file ${RANCHER_COMPOSE_LB} --pull --force-recreate
